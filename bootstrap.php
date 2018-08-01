9eb597f0b9ab857e019d836dae640b68293aab62
Serge Kukharev <serge.kukharev@gmail.com>
2018-08-01 11:48:30 +0200

    Not loading .env on production.

--- bootstrap.php
+++ bootstrap.php
@@ -2,5 +2,7 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-$dotenv = new Dotenv\Dotenv(__DIR__);
-$dotenv->load();
+if (file_exists(__DIR__ . '/.env') && getenv('ENV') !== 'production') {
+	$dotenv = new Dotenv\Dotenv(__DIR__);
+	$dotenv->load();
+}
